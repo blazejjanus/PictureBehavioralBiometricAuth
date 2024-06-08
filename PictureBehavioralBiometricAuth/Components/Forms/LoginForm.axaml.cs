1325e8af47c6a7b40a4b40dad2d9dc8226c15825
Błażej Janus <blazejjanus@gmail.com>
2024-06-08 21:32:26 +0200

    Display auth image.

---
+++ PictureBehavioralBiometricAuth/Components/Forms/LoginForm.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace PictureBehavioralBiometricAuth.Components.Forms {
+    public partial class LoginForm : UserControl {
+        public LoginForm() {
+            InitializeComponent();
+        }
+    }
+}
