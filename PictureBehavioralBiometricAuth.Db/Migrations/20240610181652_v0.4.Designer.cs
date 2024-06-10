55442ed0116458403f777aa4b6fda84aa8c990ae
Błażej Janus <blazejjanus@gmail.com>
2024-06-10 20:20:13 +0200

    new migrations

--- PictureBehavioralBiometricAuth.Db/Migrations/20240610181652_v0.4.Designer.cs
+++ PictureBehavioralBiometricAuth.Db/Migrations/20240610181652_v0.4.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PictureBehavioralBiometricAuth.Db;
@@ -11,9 +12,11 @@ using PictureBehavioralBiometricAuth.Db;
 namespace PictureBehavioralBiometricAuth.Db.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240610181652_v0.4")]
+    partial class v04
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
